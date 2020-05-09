acf6b618446d186d587ef21e5d78556e010d4475
Paul Fodrovics <pefodrovics@gmail.com>
2020-05-09 16:18:46 -0400

    redone components

---
+++ src/pages/bitcoinInfo.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { InfoConsumer } from "../components/context";
+import styled from 'styled-components';
+import Reviews from "../components/Reviews";
+import YouTube from "react-youtube"
+
+class BitcoinD extends Component {
+    render() {
+
+
+
+
+        return (
+            <div>
+                {/* // <React.Fragment> */}
+                <HeaderDetails className="container-fluid align-items-center">
+                    <h1 className="display-1 font-weight-bold">dadsfd</h1>
+                    <h4 className="display-5">dadasd</h4>
+                    <p>hellou</p>
+                    {/* Social Icons */}
+                    <div className="container mt-5">
+                        <div className="row justify-content-center">
+                            <div className="col-2">
+                                <i className="fab fa-facebook"></i>
+
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-twitter"></i>
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-google-plus-g" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-reddit" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-whatsapp" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-facebook-messenger" />
+                            </div>
+                        </div>
+                    </div>
+                </HeaderDetails>
+                {/* {Navlinks} */}
+                <div className="container">
+                    <ul className="nav nav-tabs">
+                        {/* About Place Link */}
+                        <li className="nav-item">
+                            <a href="#aboutPlace" className="nav-link active" role="tab" data-toggle="tab">About</a>
+                        </li>
+                        {/* Reviews link */}
+                        <li className="nav-item">
+                            <a href="#reviews" className="nav-link" role="tab" data-toggle="tab">Reviews</a>
+                        </li>
+                        {/* Map link */}
+                        <li className="nav-item">
+                            <a href="#map" className="nav-link" role="tab" data-toggle="tab">videos</a>
+                        </li>
+                    </ul>
+                    {/* Tab Pane */}
+                    <div className="tab-content mb-5">
+                        {/* About Place Tab */}
+                        <div id="aboutPlace" className="tab-pane in active text-center" role="tabpanel">
+                            <h2 className="mb-3">dsadas</h2>
+                            <p>hellou</p>
+                            <img src="" alt="" className="img-thumbnail img-fluid" />
+                        </div>
+                        {/* reviews */}
+                        <div className="tab-pane" id="reviews" role="tabpanel">
+                            <Reviews />
+                        </div>
+                        {/* Map */}
+                        <div className="tab-pane" id="map" role="tabpanel">
+                            <iframe src="" style={{ border: '0', height: '28.125rem', width: "100%", frameborder: '0' }}></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            // </React.Fragment>
+        );
+
+    }
+}
+        
+        
+         
+    
+export default BitcoinD;
+
+
+const HeaderDetails = styled.header`
+background: linear-gradient(rgba(109,109,109), rgba(255,255,255));
+height: 100vh;
+text-transform: uppercase;
+color: var(--mainWhite);
+text-align: center;
+
+h1 {
+    padding-top: 10%;
+    color: var(--mainDark);
+}
+
+h4 {
+    color: var(--mainDark);
+}
+p {
+    padding-left: 10%;
+    padding-right: 10%;
+    margin-bottom: 10%;
+    color: var(--mainDark);
+}
+
+i {
+    font-size: 1.875rem;
+    color: var(--mainDark);
+}
+
+i:hover {
+   color: var(--mainBlue);
+   cursor:pointer;
+}
+
+.nav-item {
+    height: 18.75rem;
+}
+
+@media(max-width: 760px) {
+    h1,h4{
+        color:var(--mainWhite)
+    }
+}
+`;
