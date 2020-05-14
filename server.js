c8d20f32f352595c16eb0a1517395dc2f171e53b
Ethan Lacy <ethanr0m818@gmail.com>
2020-05-14 11:10:39 -0400

    worked on contact page, changed folder structure

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+app.use(cors())
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -8,16 +11,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+  app.use(express.static("client/build"));
+}
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/solveremake",
-    { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
-  );
+// if (process.env.NODE_ENV === "development") {
+//   app.use(express.static("client/public"));
+// }
 
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
-  
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/solveremake",
+//   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+// );
+
+app.get("/api/contacts", (req, res) => {
+  console.log('hello');
+})
+
+// app.use(routes)
+
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
