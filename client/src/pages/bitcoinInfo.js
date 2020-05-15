907233f45d89f6b50041b5c23a2f096c5549324a
Paul Fodrovics <pefodrovics@gmail.com>
2020-05-15 12:59:30 -0400

    bitcoin info page done

--- client/src/pages/bitcoinInfo.js
+++ client/src/pages/bitcoinInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { InfoConsumer } from "../components/context";
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Reviews from "../components/Reviews";
 import YouTube from "react-youtube"
@@ -10,9 +11,15 @@ import mkb from "../components/assets/img/mkb.jpg"
 import bpc from "../components/assets/img/bpc.jpg"
 import front from "../components/assets/img/bitcoinfront.png"
 import hp from "../components/assets/img/hlvingprice.png"
+import wa from "../components/assets/img/wa.png"
+import wa1 from "../components/assets/img/wa1.png"
 import bdp from "../components/assets/img/bdp.png"
 import ledger from "../components/assets/img/ledger.svg"
 import miners from "../components/assets/img/miners.svg"
+import dg from "../components/assets/img/dg.svg"
+import bh from "../components/assets/img/bh.svg"
+import ob from "../components/assets/img/ob.svg"
+import ep from "../components/assets/img/ep.svg"
 // https://www.youtube.com/watch?v=_nBlN9yp9R8
 // https://youtu.be/_nBlN9yp9R8
 
@@ -69,7 +76,7 @@ class BitcoinD extends Component {
                             <div className="col-10 col-lg-4 mx-auto mt-4 mb-5">
                                 <div className="card" style={{ width: "18rem" }}>
                                     <h3 className="card-title text-uppercase mt-3">Digital Gold</h3>
-                                    <img src={miners} alt='Bitcoin' className="card-img-top" />
+                                    <img src={dg} alt='Bitcoin' className="card-img-top" />
                                     <div className="card-body">
 
                                         {/* <h5 className="card-title">lalal</h5> */}
@@ -82,7 +89,7 @@ class BitcoinD extends Component {
                             <div className="col-10 col-lg-4 mx-auto mt-1 mb-5">
                                 <div className="card" style={{ width: "18rem" }}>
                                     <h3 className="card-title text-uppercase mt-3">Halving</h3>
-                                    <img src={miners} alt='Bitcoin' className="card-img-top" />
+                                    <img src={bh} alt='Bitcoin' className="card-img-top" />
                                     <div className="card-body">
 
                                         {/* <h5 className="card-title">lalal</h5> */}
@@ -105,7 +112,7 @@ class BitcoinD extends Component {
                             <div className="col-10 col-lg-4 mx-auto mt-1 mb-5">
                                 <div className="card" style={{ width: "18rem" }}>
                                     <h3 className="card-title text-uppercase mt-3">Be your own Bank</h3>
-                                    <img src={miners} alt='Bitcoin' className="card-img-top" />
+                                    <img src={ob} alt='Bitcoin' className="card-img-top" />
                                     <div className="card-body">
 
                                         {/* <h5 className="card-title">lalal</h5> */}
@@ -118,38 +125,48 @@ class BitcoinD extends Component {
                             <div className="col-10 col-lg-4 mx-auto mt-1 mb-5">
                                 <div className="card" style={{ width: "18rem" }}>
                                     <h3 className="card-title text-uppercase mt-3">Part of the 1%</h3>
-                                    <img src={bdp} alt='halving' className="card-img-top" style={{width:"150px" }} />
+                                    <img src={bdp} alt='halving' className="card-img-top" style={{width:"200px" }} />
                                     <div className="card-body">
 
                                         {/* <h5 className="card-title">lalal</h5> */}
 
-                                        <p className="card-text mb-4"> Owning 1 BTC "Bitcoin" can be a very good investment. If you only own 0.28 BTC you're part of the 1% because of its fixed supply. A Bitcoin can be divided down to 8 decimal places. The smallest fraction is called a Satoshi. You don't have to buy, sell or transfer a whole bitcoin. You can start with a small amount before you get used to using a wallet and exchanges.</p>
+                                        <p className="card-text mb-5"> Owning 0.28 BTC makes you part of the 1% because of its fixed supply. A Bitcoin can be divided down to 8 decimal places. The smallest fraction is called a Satoshi. You don't have to buy, sell or transfer a whole bitcoin. You can start with a small amount before you get used to a wallet and exchanges.</p>
 
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-10 col-lg-4 mx-auto mt-1 mb-5">
+                            <div className="col-10 col-lg-4 mx-auto mt-1 ">
                                 <div className="card" style={{ width: "18rem" }}>
                                     <h3 className="card-title text-uppercase mt-3">Easy payments</h3>
-                                    <img src={miners} alt='Bitcoin' className="card-img-top" />
+                                   
+                                    <p id="wan">Wallet address: 3E9jdx5TmpGwPvV6M7itiAd33NBuJgXtoZ</p>
+                                    {/* <img src={wa1} alt='Bitcoin' className="card-img-top" style={{width:"250px", height:"100%"}} /> */}
                                     <div className="card-body">
 
                                         {/* <h5 className="card-title">lalal</h5> */}
 
-                                        <p className="card-text mb-4">Using Bitcoin for big transaction is very easy. No need for a credit card, e-mail or in some cases to reveal your identity. There are no banks to make you wait 3-7 business days for the transaction to be compleat. You just need a address or a QR code.</p>
-
+                                        <p id="wap" className="card-text mb-2 ">Using Bitcoin for big transaction is very easy. No need for a credit card, e-mail or in some cases to reveal your identity. There are no banks to make you wait 3-7 business days for the transaction to be compleat. You just need a address or a QR code to send or receive funds.</p>
+                                        <img src={wa} alt='wallet address' id="wa" className="card-img-top" style={{width:"78px", height:"100%"}} />
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-10 col-lg-4 mx-auto mt-1 mb-5">
+                            <div className="col-10 col-lg-4 mx-auto mt-1 mb-4">
                                 <div className="card" style={{ width: "18rem" }}>
-                                    <h3 className="card-title text-uppercase mt-3">Be your own Bank</h3>
-                                    <img src={miners} alt='Bitcoin' className="card-img-top" />
-                                    <div className="card-body">
+                                    <h3 className="card-title text-uppercase mt-5">Getting Started</h3>
+                                  <p>Having control over your money is a great responsibility. </p>
+                                    <div className="card-body mb-4">
+                                    <ul className="list-unstyled">
+                                        <li >First you will need a <a href="/wallets">wallet</a> </li>
+                                        
+                                    <li>Second choose an <a href="/exchanges">exchange</a> </li>
+                                    
+                                </ul>
+                          
+                                
 
                                         {/* <h5 className="card-title">lalal</h5> */}
 
-                                        <p className="card-text mb-4"> Bitcoin makes it very easy to transfer value anywhere in the world and to have control over your money. Its cryptography makes it so that nobody can take your money or make a payment on your behalf. Bitcoin can provide you with high security and financial freedom as long as you take your wallet's security seriously.  </p>
+                                        <p className="card-text mb-4">After acquiring BTC create a new address in your wallet and send yourself or to a friend/family member a small portion of Bitcoin so you can get used to the simplicity of cryptocurrencies. </p>
 
                                     </div>
                                 </div>
@@ -160,49 +177,29 @@ class BitcoinD extends Component {
 
                     </div>
 
-
-
-
-                   
-
-
-                    
-                       
-
-
-                    <p>There is a lot more to learn about Bitcoin, for now lets just take look at what a bitcoin transaction looks like. </p>
-
-                    <div className="container-fluid mt-3 ">
-                        <div className="row">
-
-                            <div className="col-sm-12 col-lg-4 mt-3 ">
-                                {/* <img src={mkb} alt='Plutus' className="card-img-top" /> */}
-                                <img src={bdp} alt='halving' className="card-img-top" />
-                                {/* <img src={mining} alt='Plutus' className="card-img-top" /> */}
-                                <p>Owning 1 BTC can be very good but if you own 0.28 BTC you're part of the 1% because of its fixed supply.  </p>
-                                <p>A Bitcoin can be divided down to 8 decimal places. The smallest fraction is called a Satoshi.</p>
+                    <div className="container mt-5">
+                        <div className="row justify-content-center">
+                            <div className="col-2">
+                                <i className="fab fa-facebook"></i>
 
                             </div>
-
-                            <div className="col-sm-12 col-lg-4 mt-3 ">
-                                <p>Usually Bitcoin has a 4 year cycle where the "Bear" market transforms into a "Bull" market, then a bubble forms and the cycle continues.</p>
-                                <p> Bitcoin doesn't care about the price fluctuation, and it continues to to be more resistent and improve because it is decentralized.  </p>
-
-                                <img src={bpc} alt='Plutus' className="card-img-top mb-2" />
-                                {/* <img src={mkb} alt='Plutus' className="card-img-top" /> */}
-                                {/* <img src={mining} alt='Plutus' className="card-img-top" /> */}
-                                <p>In our example the price of one BTC "Bitcoin" is 8,600 USD. </p>
-                                <p>Market Cap shows the size of how much money is flowing through the system at a particular time, in our case 156 Billion</p>
-                                <p>You can take a explore what other cryptocurrencies look like and find more details about their all time high and price fluctuation.<a className=" btn-outline ml-4" target="_blank" rel="noopener noreferrer" href="https://coinranking.com/">Here</a> </p>
-
+                            <div className="col-2">
+                                <i className="fab fa-twitter"></i>
                             </div>
-
-
-
+                            <div className="col-2">
+                                <i className="fab fa-google-plus-g" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-reddit" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-whatsapp" />
+                            </div>
+                            <div className="col-2">
+                                <i className="fab fa-facebook-messenger" />
+                            </div>
                         </div>
                     </div>
-
-
 
                 </HeaderDetails>
                 <br></br>
@@ -222,7 +219,7 @@ export default BitcoinD;
 
 const HeaderDetails = styled.header`
 background: linear-gradient(rgba(109,109,109), rgba(255,255,255));
-height: 700vh;
+height: 380vh;
 /* text-transform: uppercase; */
 color: var(--mainWhite);
 text-align: center;
@@ -240,6 +237,13 @@ h3 {
 h4 {
     color: var(--mainDark);
 }
+li{
+    
+    color: var(--mainDark); 
+}
+ul{
+    margin-top:-6px;
+}
 p {
     padding-left: 6%;
     padding-right: 6%;
@@ -254,6 +258,15 @@ img {
     padding-top: 4%;
     padding-left: 2%;
     padding-right: 2%;
+}
+#wa{
+    padding-top:4px;
+}
+/* #wap{
+    margin-top:-5px;
+} */
+#wan{
+    font-size: 0.82rem;
 }
 
 i {
